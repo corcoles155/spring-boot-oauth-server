431f3bffebe9d46a1541cfcd83c8f1a448e2aef6
Ana Córcoles <ana.corcoles@gmail.com>
2019-11-03 15:52:07 +0100

    Add files via upload
    Añadimos configuración de los clientes en el Servidor de Autorización.

--- src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/oauth/configuration/AuthorizationServerConfig.java
+++ src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/oauth/configuration/AuthorizationServerConfig.java
@@ -26,14 +26,33 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtAccessTokenConverter jwtAccessTokenConverter;
 
+    //Configurar los permisos que van a tener nuestros endpoints del servidor de autenticación para generar y validar el token.
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+        security.tokenKeyAccess("permitAll()") //tokenKeyAccess es el endpoint para generar el token, cualquier cliente puede acceder a esta ruta para generar el token
+        .checkTokenAccess("isAuthenticated()"); //checkTokenAccess se encarga de validar el token, isAuthenticated() nos permite validar que el cliente esté autenticado.
     }
+
+    /*
+     * Existen varios tipos de Grant Types:
+     * Password (grant_type = password)
+     * Client Credentials (grant_type = client_credentials), se utiliza para autenticación de máquin a a máquina, no se requiere permiso de un usuario específico
+     * Implicit (response_type = token), se utiliza para aplicaciones públicas que no requieran mucha seguridad.
+     * Authorization Code (response_type = code), se utiliza por lo general en páginas web, es a través de un código de autorización
+     * Authorization Code con PKCE, es la solución recomendada para aplicaciones móviles y Single Page Application (SPA).
+     * Refresh Token (grant_type = refresh_token), es necesario tener un token anterior.
+     * Access Token, suele ser un JWT
+     * */
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        super.configure(clients);
+        clients.inMemory()
+                .withClient("frontedapp") //El client_id es el identificador público de la aplicación
+                .secret(passwordEncoder.encode("12345")) //El client_secret es una contraseña o secreto que generaremos en el servidor de OAuth en relación con el cliente (la aplicación).
+                .scopes("read", "write") //El scope podría ser cualquier valor que ayude a autorizar el uso de nuestras aplicaciones.
+                .authorizedGrantTypes("password", "refresh_token") //Como vamos a obtener el token, los campos que corresponden al grant_type password son username, password, client_id, client_secret y scope
+                .accessTokenValiditySeconds(3600) //tiempo de validez del token, ponemos 1h de validez
+                .refreshTokenValiditySeconds(3600);
     }
 
     @Override
